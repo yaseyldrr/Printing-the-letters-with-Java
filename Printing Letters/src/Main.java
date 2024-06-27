c8c2ea7d3726b1a92408c31761a3270182405da9
yaseyldrr <yase.yldrr@gmail.com>
2024-06-27 22:35:53 +0300

    Printing letters top to the bottom
    Getting a text from the user and printing letters on the screen top to the bottom with Java.

---
+++ Printing Letters/src/Main.java
@@ -0,0 +1,18 @@
+import javax.lang.model.SourceVersion;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Please type a text: ");
+        String text = s.nextLine();
+        for(int i = 0; i < text.length(); i++) {
+            char letter = text.charAt(i);
+            if (letter == ' ') // if it's a space, skip
+                continue;
+                System.out.println( i + "letter = " + letter);
+        }
+    }
+}
+
+
